cf82c9fa46f1842a50b67534ef64f8665d5cc386
Jimmy <jtorres244@west-mec.org>
2021-04-15 18:21:06 -0700

    Created the skeleton for the nixon  voter

---
+++ nixons-baby.js
@@ -0,0 +1,18 @@
+var bb = document.getElementById("badButton");
+
+function first(){
+    window.setTimeout(fadeIn, 1000);
+
+}
+
+
+function fadeIn(){
+    document.getElementById('fadeIn').style.opacity = '1';
+}
+
+
+
+
+
+
+
