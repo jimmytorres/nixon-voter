dbc8aa27b43844ee00c041315ee28904c966f9fb
Jimmy <jtorres244@west-mec.org>
2021-04-16 18:08:08 -0700

    Made an onmousemove/movemouseout function/event

--- nixons-baby.js
+++ nixons-baby.js
@@ -1,17 +1,24 @@
-var bb = document.getElementById("badButton");
 
 function first(){
     window.setTimeout(fadeIn, 1000);
 
 }
 
-
 function fadeIn(){
     document.getElementById('fadeIn').style.opacity = '1';
 }
 
+function noGo(){
+    document.getElementById("badButton").innerHTML = "Wrong Button Pal";
+}
 
+function revert(){
+    document.getElementById("badButton").innerHTML = "Vote McGovern";
+}
 
+function reward(){
+    document.body.innerHTML = "You have ultimate drip B)";
+}
 
 
 
