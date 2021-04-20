37622fb772e8a3ffae97825709b4f710049cdd50
Jimmy <jtorres244@west-mec.org>
2021-04-19 18:21:50 -0700

    Added a context menu & on click event to the code

--- nixons-baby.js
+++ nixons-baby.js
@@ -1,3 +1,7 @@
+const bb = document.getElementById("badButton");
+const gg = document.getElementById("goodButton");
+const body = document.body;
+
 
 function first(){
     window.setTimeout(fadeIn, 1000);
@@ -16,10 +20,37 @@ function revert(){
     document.getElementById("badButton").innerHTML = "Vote McGovern";
 }
 
-function reward(){
-    document.body.innerHTML = "You have ultimate drip B)";
-}
+function negate(){
+    alert("uh oh loser alert lets not do that");
+    document.getElementById("papa").src = "vishawn.jpeg";
+    document.getElementById("goodButton").innerHTML = "How about now ;)";
+};
+
+bb.addEventListener('click', negate);
+
+    
+var i = 0;
+var txt = "Don't come for the Nixon pal....or else."; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function disObey(){
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+    document.getElementById("papa").style.display = 'none';
+    document.getElementById("villain").style.display = 'none';
+    document.getElementById("fadeIn").style.display = 'none';
+    bb.style.display = 'none';
+    gg.style.display = 'none';
+    
+    if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(disObey, speed);
+    }
+
+};
+
+gg.addEventListener('contextmenu', disObey);
 
 
-
-
+//Using the clientX & clientY properties
