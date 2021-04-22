ef9db0ed2667c19d4a7270ee0dc67faaaf37d619
Jimmy <jtorres244@west-mec.org>
2021-04-21 18:20:57 -0700

    Finished last 4 steps to the project

--- nixons-baby.js
+++ nixons-baby.js
@@ -2,6 +2,10 @@ const bb = document.getElementById("badButton");
 const gg = document.getElementById("goodButton");
 const body = document.body;
 
+let voter = {
+    LoserNote: "that",
+}
+
 // 2) Use an onload listener
 function first(){
     window.setTimeout(fadeIn, 1000);
@@ -23,12 +27,19 @@ function revert(){
 
 // 4) Use onclick listener
 function negate(){
-    alert("uh oh loser alert lets not do that");
+    alert(`uh oh loser alert lets not do ${voter.LoserNote}`);
     document.getElementById("papa").src = "vishawn.jpeg";
     gg.innerHTML = "How about now ;)";
+
+    erase();
 };
 
 bb.addEventListener('click', negate);
+
+function erase(){
+    bb.removeEventListener('click', negate);
+};
+
 
 // 5) Use context menu event     
 var i = 0;
@@ -38,12 +49,13 @@ var speed = 100; /* The speed/duration of the effect in milliseconds */
 function disObey(){
     body.style.backgroundColor = "black";
     body.style.color = "white";
+    body.style.cursor = "default";
     document.getElementById("papa").style.display = 'none';
     document.getElementById("villain").style.display = 'none';
     document.getElementById("fadeIn").style.display = 'none';
+    document.getElementById("helper").style.display = 'none';
     bb.style.display = 'none';
-    gg.style.display = 'none';
-    
+
     if (i < txt.length) {
         document.getElementById("demo").innerHTML += txt.charAt(i);
         i++;
@@ -57,32 +69,31 @@ gg.addEventListener('contextmenu', disObey);
 
 // 6) Use ouse over & mouse leave event listener 
 
-var enterEventCount = 0;
-var leaveEventCount = 0;
+ var enterEventCount = 0;
+ var leaveEventCount = 0;
 
-gg.addEventListener('mouseenter', e => {
-  enterEventCount++;
-  addListItem('Vote Nixon :).');
-});
+ gg.addEventListener('mouseenter', e => {
+   enterEventCount++;
+   addListItem('Vote Nixon :).');
+ });
 
-gg.addEventListener('mouseleave', e => {
-  leaveEventCount++;
-  addListItem('Vote Nixon :).');
-});
+ gg.addEventListener('mouseleave', e => {
+   leaveEventCount++;
+   addListItem('Vote Nixon :).');
+ });
 
-function addListItem(text) {
-  // Create a new text node using the supplied text
-  var newTextNode = document.createTextNode(text);
+ function addListItem(text) {
+   // Create a new text node using the supplied text
+   var newTextNode = document.createTextNode(text);
+   // Create a new li element
+   var newListItem = document.createElement("li");
+   // Add the text node to the li element
+   newListItem.appendChild(newTextNode);
+   // Add the newly created list item to list
+   gg.appendChild(newListItem);
+ };
 
-  // Create a new li element
-  var newListItem = document.createElement("li");
 
-  // Add the text node to the li element
-  newListItem.appendChild(newTextNode);
-
-  // Add the newly created list item to list
-  gg.appendChild(newListItem);
-}
 
 
 // 7) An object the moves around the page using clientX & clientY
@@ -106,4 +117,32 @@ body.onclick = function(event){
 
 
 // 8) Using "this" for onclick events
+
+
+function end(){
+    let winnerNote = "Thank you for voting Nixon the Great"
+        body.style.color = "white";
+        body.style.backgroundColor = "black";
+        body.style.cursor = "default";
+        document.getElementById("papa").style.display = 'none';
+        document.getElementById("villain").style.display = 'none';
+        document.getElementById("fadeIn").style.display = 'none';
+        document.getElementById("helper").style.display = 'none';
+        document.getElementById("demo").style.display = 'none';
+        bb.style.display = 'none';
+        gg.style.display = 'none';
+        for(let i=0; i < 10; i++){
+            var finalNote = document.createElement("h1");
+            finalNote.innerHTML += winnerNote;
+            finalNote.style.textAlign += "center";
+             body.appendChild(finalNote);
+            //document.getElementById('final').appendChild(finalNote);
+        };
+        return finalNote;
+    }
+
+gg.addEventListener('click', end);
+
+
+
 
